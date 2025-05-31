335d00f074ac9437eaf24189bf73c2caa0245b8d
filipe-576 <filipe.contato99@gmail.com>
2025-05-31 00:57:18 -0300

    leetcode206 feito

---
+++ leetcode/leetcode206/reverseLinkedList.c
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    typedef struct ListNode node;
+
+    if( head == NULL ){
+        return NULL;
+    }
+
+    node* answer = NULL;
+    node* nextNode;
+    node *current = head;
+
+    while( current != NULL ){
+        nextNode = current->next;
+
+        current->next = answer;
+        answer = current;
+
+        current = nextNode;
+
+    }
+
+    return answer;
+
+}
